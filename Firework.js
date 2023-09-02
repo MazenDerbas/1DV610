1f2676722668672863efc2c422dca101cee5ac7e
md222zv <89812908+Mazender@users.noreply.github.com>
2023-09-02 18:59:30 +0200

    Add movement to the firework

--- Firework.js
+++ Firework.js
@@ -2,15 +2,41 @@ const submit = document.getElementById('generateButton')
 
 export class Firework{
     constructor (){
-
         this.fireWorkDiv = document.createElement('div');
         this.fireWorkDiv.classList.add('firework');
         document.body.appendChild(this.fireWorkDiv);
+
+        this.positionX = window.innerWidth / 2; //Place the star in the center of the browser 
+        this.positionY = window.innerHeight - 10; // subtract 10 pixels form the hight so the star will apper 
+
+        this.angle = (Math.random() * Math.PI * 2);
+        this.vx = Math.cos(this.angle) * 5 //speed is 5
+        this.vy = Math.sin(this.angle) * 5
+
+        this.fireWorkDiv.style.left = this.positionX + 'px';
+        this.fireWorkDiv.style.top = this.positionY   + 'px';
+    
     }
+
+
+move() {
+    this.positionX = this.positionX + this.vx // update the position x
+    this.positionY = this.positionY + this.vy // update the position y
+    this.fireWorkDiv.style.left= this.positionX + 'px'; 
+    this.fireWorkDiv.style.top = this.positionY + 'px'; 
+    
 }
 
+
+}
+const firework = new Firework ();
+
 submit.addEventListener('click', ()=>  {
-    const firework = new Firework ();
 });
+    setInterval(() => {
+        firework.move()
+      }, 10);
+    
+
 
   
